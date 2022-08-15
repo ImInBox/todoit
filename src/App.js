d246930cdf8243c7d01eee0b69e62155106ce14b
I'm In Box <iminbox24dev@gmail.com>
2022-08-15 14:55:58 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from 'react'
 import LoginPage from './pages/Login'
 import SignUpPage from './pages/Signup'
 import HomePage from './pages/Home'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
-      <LoginPage></LoginPage>
-      <SignUpPage></SignUpPage>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
     </div>
   );
 }
