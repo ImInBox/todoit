cdb62b63b669c81bf343f0700508db38764ebb2a
I'm In Box <iminbox24dev@gmail.com>
2022-08-15 22:22:46 +0200

    Login and SignUp pages frontend update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,12 @@
+import React from "react";
+
 function LoginPage(){
     return(
-        <p className="text-blue-500">LOGIN</p>
+        <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <div className="bg-white h-[24rem] w-[24rem] rounded-xl text-center">
+                <h1 className="text-[50px]">LOGIN</h1>
+            </div>
+        </div>
     )
 }
 export default LoginPage;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,10 @@
 function SignUpPage(){
     return(
-        <p className="text-blue-500">SIGN UP</p>
+        <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <div className="bg-white h-[24rem] w-[24rem] rounded-xl text-center">
+                <h1 className="text-[50px]">SIGN UP</h1>
+            </div>
+        </div>
     )
 }
 export default SignUpPage;
