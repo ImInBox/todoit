5b5f9bb6c0fa4a2d60b4ecaf67fe02601f42b849
I'm In Box <iminbox24dev@gmail.com>
2022-08-18 23:21:31 +0200

    home page small changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,18 @@
+import React from "react";
+import Button from "../components/Button";
+
 function HomePage(){
     return(
-        <p className="text-purple-500">HOME</p>
+        <div className="p-[15%] h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">   
+            <div className="flex flex-wrap h-full w-full border-dashed border-2 border-black drop-shadow-2xl bg-white rounded-xl text-center">
+                <div className="h-full w-[50%]">
+
+                </div>
+                <div className="h-full w-[50%] flex flex-col items-center">
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 export default HomePage;
