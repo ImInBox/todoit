a26e8c9cdc2220f630ffe4efacb29d0c0e511bd6
I'm In Box <iminbox24dev@gmail.com>
2022-08-20 20:51:19 +0200

    added button navigate attribute

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 function Button(props){
+    const navigate = useNavigate();
+
     return(
-        <button className={`${props.className} text-white bg-black hover:scale-105 focus:scale-105 focus:outline-none duration-200 w-[18rem] h-10 mt-8 text-center rounded-md`}>{props.text}</button>
+        <button onClick={() => navigate(props.navigate)} className={`${props.className} text-white bg-black hover:scale-105 focus:scale-105 focus:outline-none duration-200 w-[18rem] h-10 mt-8 text-center rounded-md`}>{props.text}</button>
     )
 }
 
