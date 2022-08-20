a982c3b1964779aee2a652a1195bfcb122c30595
I'm In Box <iminbox24dev@gmail.com>
2022-08-20 20:51:33 +0200

    Home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import Background from "../components/Background";
 import Frame from "../components/Frame";
+import Button from "../components/Button";
 
 function HomePage(){
     return(
-        <Background className="p-[15%]">   
-            <Frame className="flex flex-wrap h-full w-full">
-                <div className="h-full w-[50%]"/>
-                <div className="h-full w-[50%] flex flex-col items-center"/>
+        <Background className="flex justify-center items-center">   
+            <Frame className="flex h-[32rem] w-[54rem] p-8">
+
+                <Frame className="text-center h-full w-full mr-8">
+                    <h1 className="pt-8 text-[50px]">Welcome</h1>
+                </Frame>
+                
+                <Frame className="text-center h-full w-full flex items-center flex-col">
+
+                    <h1 className="pt-8 text-[50px]">You Can</h1>
+                    <Button navigate="/login" text="Login" className="mb-2"/>
+                    <h2 className="text-[20px]">OR</h2>
+                    <Button navigate="/signup" text="Signup" className="mt-2 mb-8"/>
+                    <Frame className="text-center">
+                        <h2 className="text-[30px] font-medium">Disclaimer!</h2>
+                        <p className="px-3 pb-1">todoit is now in beta!<br/>To sign in you will need <br/>"Beta token" given by administrator</p>
+                    </Frame>
+                    
+                </Frame>
+
             </Frame>
         </Background>
     )
