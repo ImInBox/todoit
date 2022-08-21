fbfd6167d9f9e4951e08886c24b6097fb4dfab73
I'm In Box <iminbox24dev@gmail.com>
2022-08-21 19:11:28 +0200

    added pading to input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 function Input(props){
     return(
-        <input className={`{props.className} focus:scale-105 hover:scale-105 focus:outline-none duration-200 w-[18rem] h-10 mt-8 text-center rounded-md border-dashed border-black border-2`} placeholder={props.placeholder} type={props.type}/>
+        <input className={`${props.className} p-[8px] focus:scale-105 hover:scale-105 focus:outline-none duration-200 w-[18rem] h-10 mt-8 text-center rounded-md border-dashed border-black border-2`} placeholder={props.placeholder} type={props.type}/>
     )
 }
 
