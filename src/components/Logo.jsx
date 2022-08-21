243afd0eba05469ea076264c7100709816cf6e67
I'm In Box <iminbox24dev@gmail.com>
2022-08-21 19:11:49 +0200

    added Logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+function Logo(){
+    return(
+        <div className="text-cente h-mini w-mini">
+            <h2 className="font-kanit text-[45px]">TODO</h2>
+            <h1 className="font-kanit text-[140px] -translate-y-20">IT</h1>
+        </div>
+    )
+}
+
+export default Logo;
