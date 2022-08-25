89d3e8439a5f833955956daf7f596cf1c8ff7fd6
I'm In Box <iminbox24dev@gmail.com>
2022-08-25 20:27:30 +0200

    Sidebar changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import { CgMenu } from "react-icons/cg"
+import { CgMenu, CgClose} from "react-icons/cg"
 
 function Sidebar(){
     const [isOpen, setIsOpen] = useState(false)
 
     return(
         <>
-        <div className={`duration-300 top-0 right-0 bg-white fixed ${isOpen ? "w-[300px]" : "w-[56px]"} h-full p-2`}>
-            <i><button onClick={() => setIsOpen(!isOpen)}><CgMenu size={40}/> </button><h2 className={`text-2xl ${isOpen ? "" : "hidden"}`}>sidebar</h2></i>
+        <div className={`rounded-r duration-300 top-0 left-0 bg-white fixed ${isOpen ? "" : "-translate-x-[300px]"} h-full`}>
+            <div className="flex h-full w-[300px]">
+                <h2 className="ml-8 font-kanit text-[60px]">TODO IT</h2>
+            </div>
         </div>
+        <button className={`rounded duration-300 hover:text-white hover:bg-gray-800 m-4 top-0 left-0 bg-white fixed ${isOpen ? "translate-x-[300px]" : ""} p-2`} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <CgClose size={40}/> : <CgMenu size={40}/>}
+        </button>
         </>
     )
 }
