6b3d9823c6637356753a7dbdeb714b35d8b1f285
I'm In Box <iminbox24dev@gmail.com>
2022-08-26 18:22:24 +0200

    sidebar hover bg-color update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ function Sidebar(){
                 <h1 className="ml-8 mb-2 font-kanit text-[60px]">TODO IT</h1>
                 <ul>
                     {sidebarItems.map((item, index) => (
-                        <li key={index} onClick={() => navigate(item.link)} className="rounded duration-100 hover:bg-slate-700 hover:text-white m-2 p-2 flex items-center cursor-pointer">
+                        <li key={index} onClick={() => navigate(item.link)} className="rounded duration-100 hover:bg-black hover:text-white m-2 p-2 flex items-center cursor-pointer">
                             <IconContext.Provider value={{size: "25", className: "m-[5px] duration-100"}}>
                                 {item.icon}
                             </IconContext.Provider>
@@ -37,7 +37,7 @@ function Sidebar(){
                 </ul>
             </div>
         </div>
-        <button  onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? "translate-x-[300px]" : ""} group px-2 py-3 rounded duration-300 hover:text-white hover:bg-slate-700 m-4 top-0 left-0 bg-white fixed`}>
+        <button  onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? "translate-x-[300px]" : ""} group px-2 py-3 rounded duration-300 hover:text-white hover:bg-black m-4 top-0 left-0 bg-white fixed`}>
             <div className={`${isOpen ? "-rotate-45 translate-y-[11px]" : ""} rounded-full duration-300 group-hover:bg-white bg-black h-[5px] w-[35px]`}></div>
             <div className={`${isOpen ? "opacity-0" : ""} rounded-full duration-300 group-hover:bg-white bg-black my-[6px] h-[5px] w-[35px]`}></div>
             <div className={`${isOpen ? "rotate-45 -translate-y-[11px]" : ""} rounded-full duration-300 group-hover:bg-white bg-black h-[5px] w-[35px]`}></div>
