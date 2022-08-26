c39bd8dda7f44e0b904970e87056d717b2f5c878
I'm In Box <iminbox24dev@gmail.com>
2022-08-26 13:43:53 +0200

    added hamburger animation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { CgMenu, CgClose} from "react-icons/cg"
 
 function Sidebar(){
     const [isOpen, setIsOpen] = useState(false)
@@ -11,8 +10,10 @@ function Sidebar(){
                 <h2 className="ml-8 font-kanit text-[60px]">TODO IT</h2>
             </div>
         </div>
-        <button className={`rounded duration-300 hover:text-white hover:bg-gray-800 m-4 top-0 left-0 bg-white fixed ${isOpen ? "translate-x-[300px]" : ""} p-2`} onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <CgClose size={40}/> : <CgMenu size={40}/>}
+        <button  onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? "translate-x-[300px]" : ""} group px-2 py-3 rounded duration-300 hover:text-white hover:bg-gray-800 m-4 top-0 left-0 bg-white fixed`}>
+            <div className={`${isOpen ? "-rotate-45 translate-y-[11px]" : ""} rounded-full duration-300 group-hover:bg-white bg-black h-[5px] w-[35px]`}></div>
+            <div className={`${isOpen ? "opacity-0" : ""} rounded-full duration-300 group-hover:bg-white bg-black my-[6px] h-[5px] w-[35px]`}></div>
+            <div className={`${isOpen ? "rotate-45 -translate-y-[11px]" : ""} rounded-full duration-300 group-hover:bg-white bg-black h-[5px] w-[35px]`}></div>
         </button>
         </>
     )
